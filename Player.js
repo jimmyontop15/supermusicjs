57b4f42339842b198a614f3cce8ed8cf7feb5988
jimmyontop15 <fxwfzofficial@gmail.com>
2025-03-14 12:39:38 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,14 +1,33 @@
+const Queue = require("./Queue");
+
 class Player {
   constructor(nodeManager, send) {
     this.nodeManager = nodeManager;
-    this.queue = [];
+    this.queue = new Queue();
     this.currentTrack = null;
     this.send = send;
   }
 
   async play(track) {
+    if (this.currentTrack) {
+      this.queue.add(track);
+      return;
+    }
+
     this.currentTrack = track;
     await this.nodeManager.request("/v4/sessions/play", { method: "POST", body: JSON.stringify({ track }) });
+
+    this.checkQueue();
+  }
+
+  async checkQueue() {
+    if (this.queue.isEmpty()) {
+      this.currentTrack = null;
+      return;
+    }
+
+    this.currentTrack = this.queue.next();
+    await this.nodeManager.request("/v4/sessions/play", { method: "POST", body: JSON.stringify({ track: this.currentTrack }) });
   }
 
   pause() {
@@ -20,6 +39,7 @@ class Player {
   }
 
   stop() {
+    this.queue.clear();
     this.currentTrack = null;
     return this.nodeManager.request("/v4/sessions/stop", { method: "POST" });
   }
